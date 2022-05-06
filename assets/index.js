453c738466aa26f973a550525cf2972307423b55
Paulm1999 <99810619+Paulm1999@users.noreply.github.com>
2022-05-06 02:16:47 +0100

    last update

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+document.getElementById("check1").checked = true;
+document.getElementById("check2").checked = false;
+document.getElementById("check3").checked = true;
+document.getElementById("check4").checked = false;
+document.getElementById("check5").checked = true;
+document.getElementById("check6").checked = true;
+
+document.getElementById("p2").style.backgroundColor = " #2196f3";
