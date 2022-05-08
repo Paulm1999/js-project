a7156f1b595f1d1b483198232acd9e41d7079b3c
Paulm1999 <99810619+Paulm1999@users.noreply.github.com>
2022-05-09 00:40:32 +0100

    last update

--- assets/index.js
+++ assets/index.js
@@ -4,5 +4,3 @@ document.getElementById("check3").checked = true;
 document.getElementById("check4").checked = false;
 document.getElementById("check5").checked = true;
 document.getElementById("check6").checked = true;
-
-document.getElementById("p2").style.backgroundColor = " #2196f3";
