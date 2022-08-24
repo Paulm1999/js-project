b2f2c4c5f3b35cd6a102ae8923e7f1efc3167cf3
Paulm1999 <99810619+Paulm1999@users.noreply.github.com>
2022-08-24 14:39:53 +0100

    last update

--- assets/index.js
+++ assets/index.js
@@ -12,3 +12,11 @@ products.forEach((item) => {
 });
 
 wrapper.innerHTML = cards;
+
+var card1 = [{ title, image, rating, inStore, weight, price, oldPrice }];
+var card2 = [{ title, image, rating, inStore, weight, price, oldPrice }];
+var card3 = [{ title, image, rating, inStore, weight, price, oldPrice }];
+var card4 = [{ title, image, rating, inStore, weight, price, oldPrice }];
+var card5 = [{ title, image, rating, inStore, weight, price, oldPrice }];
+var card6 = [{ title, image, rating, inStore, weight, price, oldPrice }];
+var
